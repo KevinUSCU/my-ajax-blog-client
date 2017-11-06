ddafc78cf10cb12b5983eac93244673bc90dbed7
Kevin Springer <KevinUSCU@outlook.com>
2017-11-06 12:50:43 -0800

    Add new/update post form template.

---
+++ scripts/form-template.js
@@ -0,0 +1,13 @@
+postForm = `
+  <form>
+    <div class="form-group">
+      <label for="postTitle">Post Title</label>
+      <input type="text" class="form-control" id="postTitle" placeholder="title">
+    </div>
+    <div class="form-group">
+      <label for="postContent"></label>
+      <input type="text" class="form-control" id="postContent" placeholder="type your post here">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+`
