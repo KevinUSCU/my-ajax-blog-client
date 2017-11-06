3c78640668efe931bac570bb7f815c88f2036f25
Kevin Springer <KevinUSCU@outlook.com>
2017-11-06 12:56:31 -0800

    Update template form titles.

--- scripts/form-template.js
+++ scripts/form-template.js
@@ -1,11 +1,11 @@
 postFormTemplate = `
   <form>
     <div class="form-group">
-      <label for="postTitle">Post Title</label>
+      <label for="postTitle">Title</label>
       <input type="text" class="form-control" id="postTitle" placeholder="title">
     </div>
     <div class="form-group">
-      <label for="postContent"></label>
+      <label for="postContent">Content</label>
       <input type="text" class="form-control" id="postContent" placeholder="type your post here">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
