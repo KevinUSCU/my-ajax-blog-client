ea25b097c115c762a5ff2baa73119b6f3b40d76a
Kevin Springer <KevinUSCU@outlook.com>
2017-11-07 00:25:38 -0800

    Test alternate baseURL.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://git.heroku.com/kevinuscu-my-ajax-blog-server.git'
+const baseURL = 'https://git.heroku.com/kevinuscu-my-ajax-blog-server.git:3000'
 // const baseURL = 'http://localhost:3000'
 
 // On page load, get content reference and display index
